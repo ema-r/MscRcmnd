02f423f71278f6ffdb8dcfd219adb975c1b9d9f5
Emanuele <103453667+ema-r@users.noreply.github.com>
2024-03-08 16:09:17 +0100

    Create app.py

---
+++ businesslogic/app.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+import mysql.connector
+
+
+class DBManager:
+    def __init__(self, database='test', host="db", user="root", password_file=None):
+        pf = open(password_file, 'r')
+        self.connection = mysql.connector.connect(
+            user=user, 
+            password=pf.read(),
+            host=host, # name of the mysql service as set in the docker compose file
+            database=database,
+            auth_plugin='mysql_native_password'
+        )
+        pf.close()
+        self.cursor = self.connection.cursor()
+    
+    def populate_db(self):
+        self.cursor.execute('DROP TABLE IF EXISTS example')
+        self.cursor.execute('CREATE TABLE example)')
+        self.connection.commit()
+
+
+server = Flask(__name__)
+conn = None
+
+@server.route('/')
+def hello():
+    global conn
+    if not conn:
+        conn = DBManager(password_file='/run/secrets/db-password')
+        conn.populate_db()
+    return "db setup\n"
+
+
+if __name__ == '__main__':
+    server.run()
