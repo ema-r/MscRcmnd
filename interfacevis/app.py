212e916fe991e19c399e47abd62fc7bf70181318
erem1x <martino.1715650@studenti.uniroma1.it>
2024-03-24 17:05:55 +0100

    fixed signup form

--- interfacevis/app.py
+++ interfacevis/app.py
@@ -23,7 +23,7 @@ def contact():
         email = request.form['email']
         message = request.form['message']
 
-        # Process the form data (you can add your own logic here)
+        #(add own logic here)
 
         # For demonstration purposes, let's print the form data
         print(f"Name: {name}")
@@ -43,35 +43,32 @@ def contact():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Ottenere i dati inviati dalla form di registrazione
-        username = request.form['name']
+        # Get form data
+        username = request.form['username']
         email = request.form['email']
         password = request.form['password']
 
-        # Esegui il codice per salvare i dati dell'utente nel database o fare altre operazioni necessarie per la registrazione
+        return jsonify({"Username": username, "Email": email})
 
-        # Di solito, reindirizziamo l'utente a una pagina di conferma dopo la registrazione
-        #return render_template('signup_confirmation.html', username=username)
     else:
-        # Se la richiesta non è di tipo POST, restituisci semplicemente la pagina di registrazione
+        # If it's GET, return normal page
         return render_template('signup.html', active_page='signup')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Ottieni i dati inviati dalla form di login
+        # Get form data
         username = request.form['username']
         password = request.form['password']
 
-        # Esegui il codice per verificare le credenziali dell'utente nel database o in altro modo
-        # In questo esempio, controlliamo solo se le credenziali sono corrette
+        # Check input data 
         if username == 'admin' and password == 'password':
             return "Login successful! Welcome, {}".format(username)
         else:
             return "Login failed. Please check your username and password."
 
-    # Se la richiesta non è di tipo POST, restituisci semplicemente la pagina di login
+    # GET request
     return render_template('login.html', active_page='login')
 
 if __name__ == "__main__":
