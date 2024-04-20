2a4f98d1ce5692e71443e530389765c1c988a3ac
Emanuele <ema@localhost.localdomain>
2024-04-20 13:16:37 +0200

    New container that takes care of spotify api calls

---
+++ interfacespot/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return jsonify({'hello': 'world'})
+
+
+if __name__ == '__main__':
+    app.run()
