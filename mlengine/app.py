a3bb7a29de32d419f1cabe6a09b8f884f3a825ef
ema-r <ema@localhost.localdomain>
2024-08-23 17:26:31 +0200

    Committer: ema-r <ema@localhost.localdomain>
     On branch main
     Your branch is up to date with 'origin/main'.

--- mlengine/app.py
+++ mlengine/app.py
@@ -1,13 +1,29 @@
 from pyspark import SparkConf, SparkContext
 from flask import Flask, jsonify, request
 
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import select
+
+# conf spark session
 conf = SparkConf()
 conf.setMaster("local")
 conf.setAppName("SparkContext1")
 conf.set("spark.executor.memory", "1g")
 
+# conf sql session
+engine = sqlalchemy.create_engine("mariadb+mariadbconnector://test_user:test@db:3306/test_database")
+Base = declarative_base()
+
+Session = sqlalchemy.orm.sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+# start server
 app = Flask(__name__)
 
+# needs query on user id, and reccomendations from the user. We need to add a music id table
 
 if __name__ == '__main__':
     app.run()
