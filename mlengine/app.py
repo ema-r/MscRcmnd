a71b1a897065d35f55c29ae29407f319e9b9c9b1
ema-r <ema@localhost.localdomain>
2024-10-19 00:19:58 +0200

    Committer: ema-r <ema@localhost.localdomain>
     On branch ml
     Changes to be committed:
    	modified:   .gitignore
    	modified:   mlengine/app.py

--- mlengine/app.py
+++ mlengine/app.py
@@ -1,25 +1,55 @@
 from flask import Flask, jsonify, request
 
-import sqlalchemy
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import select
+import numpy
+import pandas
 
-# conf sql session
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://test_user:test@db:3306/test_database")
-Base = declarative_base()
-
-Session = sqlalchemy.orm.sessionmaker()
-Session.configure(bind=engine)
-session = Session()
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.metrics import euclidean_distances
+from scipy.spatial.distance import cdist
 
 # start server
 app = Flask(__name__)
 
+# Columns to use
+columnIndexes = ['valence', 'acousticness', 'danceability', 'energy',
+                 'instrumentalness', 'speechiness', 'liveness','tempo', 'key']
+
 # needs query on user id, and reccomendations from the user. We need to add a music id table
-@app.route('/get_reccomandation/<int:user_id>')
-def get_rec(user_id):
-    return jsonify({'message': 'Reccomendation successfully added'}), 200
+@app.route('/get_reccomandation/<str:song_title>&<str:song_artist>')
+def get_rec(song_title, song_artist):
+    
+    data = pandas.read_csv("./data.csv")
+    
+    metadataCols = ['name', 'year', 'artists']
+    
+    song_data = get_song_data_from_csv(song_title, song_artist, data)
+
+    songVector   = get_song_vector(song_data) 
+
+    scaler = song_clustering_pipeline.steps[0][1]
+    scaled_data = scaler.transform(data[columnIndexes])
+    scaled_song_center = scaler.transform(songVector.reshape(1, -1))
+    distances = cdist(scaled_song_center, scaled_data, 'cosine')
+    index = list(np.argsort(distances)[:, :n_songs][0])
+
+    rec_song = data.iloc[index]
+
+    return jsonify({'message': rec_song[columnIndexes].to_dict(orient='records')}), 200
+
+def get_song_data_from_csv(song_name, song_artist, csv_data):
+    try:
+        song_data = csv_data[(csv_data['name'] == song_name)] & csv_data[(csv_data['artists'] == song_artist)]
+        return song_data
+
+    except IndexError:
+        return jsonify("error")
+
+def get_song_vector(song_data):
+    return song_vector = song_data[columnIndexes].values
 
 if __name__ == '__main__':
     app.run()
