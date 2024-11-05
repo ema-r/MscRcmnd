aada5afc5445249bb8ead57aa3a1ce0d396ea2d6
erem1x <41081204+erem1x@users.noreply.github.com>
2024-11-05 17:40:59 +0100

    Update spotify_secrets.py with new disposal API

--- interfacespot/spotify_secrets.py
+++ interfacespot/spotify_secrets.py
@@ -1,7 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-cid = '8cbf98df7ac84b1a86633c4da3178f2f'
-secret = '980a89d78db24fc8bc8e36cfe2c2abe7'
+cid = '458c511cb460490c87984d079bc5a290'
+secret = '7ca35c86a8da4b688e14aa9514405b3b'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
