d66c3e27583e0de04110230b5777d979d6b633ec
erem1x <41081204+erem1x@users.noreply.github.com>
2024-11-06 19:29:17 +0100

    Update spotify_secrets.py

--- interfacespot/spotify_secrets.py
+++ interfacespot/spotify_secrets.py
@@ -1,7 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-cid = '458c511cb460490c87984d079bc5a290'
-secret = '7ca35c86a8da4b688e14aa9514405b3b'
+cid = ''
+secret = ''
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
